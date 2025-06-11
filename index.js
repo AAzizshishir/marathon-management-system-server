971caf3a8ed7615eea6d3a6788fcc9982eb56301
shishir <mdabdulaziz3745@gmail.com>
2025-06-11 19:58:42 +0600

    initialized server with express and connented to the mongodb

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  console.log("");
+  res.send("Marathon Server is running...");
+});
+console.log(process.env.db_marathon);
+console.log(process.env.db_password);
+
+const uri = `mongodb+srv://${process.env.db_marathon}:${process.env.db_password}@cluster0.pacddgd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log("Port is running on port", port);
+});
